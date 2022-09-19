ec24a8c0b3b51987da6b4b1841c0f84ab5cbda1d
Stanislav Korolkov <stasevich741@gmail.com>
2022-09-19 23:05:13 +0300

    init

--- ToDo-JDBC/src/main/java/com/example/ToDoJDBC.java
+++ ToDo-JDBC/src/main/java/com/example/ToDoJDBC.java
@@ -3,7 +3,7 @@ package com.example;
 /**
  * Hello world!
  */
-public class App {
+public class ToDoJDBC {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ ToDo_client/src/main/java/com/example/ToDoClient.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class ToDoClient {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
